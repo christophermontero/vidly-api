e7dda17d3c5f8f1f6470a2ba254126500d3904bb
christophermontero <chris.to20@hotmail.com>
2021-06-10 16:20:56 -0500

    Create an static method to look up a rental

--- src/models/rental-model.js
+++ src/models/rental-model.js
@@ -1,63 +1,70 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const Rental = mongoose.model(
-  'Rental',
-  new mongoose.Schema({
-    customer: {
-      type: new mongoose.Schema({
-        name: {
-          type: String,
-          required: true,
-          minlength: 2,
-          maxlength: 255
-        },
-        isGold: {
-          type: Boolean,
-          default: false
-        },
-        phone: {
-          type: String,
-          required: true,
-          minlength: 7,
-          maxlength: 10
-        }
-      }),
-      required: true
-    },
-    movie: {
-      type: new mongoose.Schema({
-        title: {
-          type: String,
-          minlength: 2,
-          maxlength: 255,
-          trim: true,
-          lowercase: true,
-          required: true
-        },
-        dailyRentalRate: {
-          type: Number,
-          required: true,
-          min: 0,
-          max: 255
-        }
-      }),
-      required: true
-    },
-    dateOut: {
-      type: Date,
-      required: true,
-      default: Date.now
-    },
-    dateReturned: {
-      type: Date
-    },
-    rentalFee: {
-      type: Number,
-      min: 0
-    }
-  })
-);
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 255
+      },
+      isGold: {
+        type: Boolean,
+        default: false
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 7,
+        maxlength: 10
+      }
+    }),
+    required: true
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        minlength: 2,
+        maxlength: 255,
+        trim: true,
+        lowercase: true,
+        required: true
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+      }
+    }),
+    required: true
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  dateReturned: {
+    type: Date
+  },
+  rentalFee: {
+    type: Number,
+    min: 0
+  }
+});
+
+// Static method
+rentalSchema.statics.lookup = function (customerId, movieId) {
+  return this.findOne({
+    'customer._id': customerId,
+    'movie._id': movieId
+  });
+};
+
+const Rental = mongoose.model('Rental', rentalSchema);
 
 // Validation customer schema
 function validateRental(rental) {
