66e4ac6e4454b2f2d6f47fd82c5da54d2983cdcb
christophermontero <chris.to20@hotmail.com>
2021-06-12 15:33:49 -0500

    Transporters to write logs in files are commented

--- src/startup/logging.js
+++ src/startup/logging.js
@@ -4,24 +4,34 @@ const config = require('config');
 require('express-async-errors');
 
 module.exports = function (logger, app) {
-  logger.exceptions.handle(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+  logger.exceptions.handle(
+    new winston.transports.MongoDB({
+      db: config.get('db'),
+      level: 'info',
+      options: { useUnifiedTopology: true }
+    })
+  );
+
+  // logger.exceptions.handle(new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
 
   process.on('unhandledRejection', (ex) => {
     throw ex;
   });
 
-  winston.add(
-    new winston.transports.File({
-      filename: 'logfile.log',
-      format: winston.format.combine(winston.format.timestamp(), winston.format.prettyPrint())
-    })
-  );
+  // winston.add(
+  //   new winston.transports.File({
+  //     filename: 'logfile.log',
+  //     format: winston.format.combine(winston.format.timestamp(), winston.format.prettyPrint())
+  //   })
+  // );
+
   winston.add(
     new winston.transports.Console({
       format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
       handleExceptions: true
     })
   );
+
   if (app.get('env') !== 'test') {
     winston.add(
       new winston.transports.MongoDB({
